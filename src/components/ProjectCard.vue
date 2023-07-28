1b55853eb7d7bb9db9785ad33c0e0b038a50545d
Samira Gibson <saga44studio@gmail.com>
2023-07-27 22:11:04 -0400

    pr update

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="card">
-    <div>
+    <h3>{{ title }}</h3>
+    <div class="skif">
       <div class="card-image">
         <img :src="imageSrc" alt="Project Image" />
       </div>
-      <div class="projects">
-        <h3>{{ title }}</h3>
+      <div class="card-content">
+        <!--<h3>{{ title }}</h3>-->
         <p class="desc">{{ desc }}</p>
       </div>
     </div>
@@ -37,8 +38,9 @@ export default {
   box-sizing: border-box;
 }
 h3 {
-  margin: 20px 0 0;
+  margin: 20px 0 -20px 0;
   padding: 0 20px;
+  align-self: center;
 }
 ul {
   list-style-type: none;
@@ -52,8 +54,8 @@ a {
   color: $tertiary-color;
 }
 p {
-  padding: 0 15px;
-  margin-top: 20px;
+  padding: 0 1.3rem 0 0.2rem;
+  //margin-top: 20px;
   margin-bottom: 20px;
 }
 
@@ -66,7 +68,6 @@ p {
   border-radius: 5px;
   color: #000000;
   text-shadow: rgba(0, 0, 0, 0.3) 0px 1px 5px;
-  /* glassmorphism background*/
   background: $mira-blue;
   border-radius: 80px;
   border-bottom: 23px solid rgb(0, 0, 0);
@@ -74,30 +75,56 @@ p {
   border-top: 6px solid rgb(0, 0, 0);
   border-left: 9px solid rgb(0, 0, 0);
   //keep .hello vertically centered
-  display: block;
+  display: flex;
+  flex-direction: column;
+  //grid-template-columns: 1fr 2fr;
+  //flex-wrap: wrap;
   max-width: 80vw;
   max-height: 75vh;
   text-align: left;
   overflow: hidden;
   z-index: 0;
-  margin: 7rem 0;
+  //margin: 7rem 0;
 }
 
 .card-image {
   vertical-align: middle;
-  margin-right: 0.5rem;
-  padding-bottom: 0.2rem;
-  padding: 5px;
-  display: inline-block;
+  //margin-right: 0.5rem;
+  //padding-bottom: 0.2rem;
+  padding: 1rem 0 0 2rem;
+  width: 120px;
+  left: 0;
+  height: 100px;
+  //margin-bottom: 0;
+  // make image fill it's container
+  object-fit: cover;
+  display: inline-flex;
+  flex-direction: row;
+  //overflow: hidden;
+}
+.card-content {
+  margin-left: 0.5rem;
+  width: 65%;
+  //display: inline-block;
+  //float: right;
+  flex-direction: row;
+  display: inline-flex;
+  //margin-top: 0%;
 }
 img {
-  width: 10vw;
+  width: 100%;
   height: 100%;
   padding: 5px;
+  margin-top: 1.2rem;
+  border-radius: 20% 20% 20% 20%;
 }
 h1 {
   margin-left: 3%;
   margin-bottom: 3%;
+}
+.skif {
+  margin: 0;
+  padding: 0%;
 }
 
 @media (max-width: 768px) {
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="project">
-    <div>
-      <ProjectComponent />
-    </div>
+  <div class="projects">
+    <ProjectComponent />
     <div id="project-one">
       <ProjectCard
+        class="project-card this"
         imageSrc="https://via.placeholder.com/100"
         title="The Philly We Were Promised"
         desc="An interactive map view showing how Philadelphia could've looked if the city had followed through on its plans to revitalize in the early 2000s."
       />
     </div>
-    <div class="project-two">
+    <div id="project-two">
       <ProjcetCard
+        class="project-card that"
         imageSrc="https://via.placeholder.com/100"
         title="Philly Icons We Loved in 2022"
         desc="A Philadelphia Inquirer interactive poll of the city's favorite icons, from Quinta Brunson to Gritty. Who's your fave of 2022? "
@@ -43,7 +43,7 @@ export default {
   box-sizing: border-box;
 }
 
-.project {
+.projects {
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -72,5 +72,33 @@ p {
 }
 h1 {
   padding: 1.5rem 15px 0rem 3rem;
+}
+#projects-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  //margin-top: 60%;
+  //min-width: 60vw;
+  //left: 25%;
+  //height: 100vh;
+}
+.project-card {
+  position: absolute;
+  margin-bottom: 20px;
+  padding: 5px 0;
+}
+.this {
+  margin-bottom: 3rem;
+  margin-top: 13rem;
+}
+.that {
+  margin-top: 60%;
+}
+@media (max-width: 768px) {
+  p {
+    padding: 0 8px !important;
+  }
 }
 </style>
